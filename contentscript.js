92d476ea9e8e6e837671658e59c495512f920de0
uclinxiuda <wb-lxd188077@alibaba-inc.com>
2016-06-30 15:11:22 +0800

    'add'

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,10 @@
 chrome.extension.onMessage.addListener(function(request) {
 	var data = request.match(/data:(.*)/)[1];
 	//document.body.innerHTML = data
-	changeColor(data);
+	//changeColor(data);
+	[].forEach.call($("*"), function(a) {
+		a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16)
+	})
 });
 
 function changeColor(color) {
