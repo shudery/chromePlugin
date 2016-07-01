56377794a233e889c46b3c9f40455638477a4edf
shudery <125153450@qq.com>
2016-07-01 23:48:17 +0800

    'logo'

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,30 @@
-chrome.extension.onMessage.addListener(function(request) {
-	var data = request.match(/data:(.*)/)[1];
-	//document.body.innerHTML = data
-	//changeColor(data);
-	[].forEach.call($("*"), function(a) {
-		a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16)
-	})
+var functionHash = {
+	outlineFun() {
+		[].forEach.call($("*"), function(a) {
+			a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16)
+		})
+		console.log('add color outline')
+	},
+	addLinkTarget() {
+		[].forEach.call($('a'),function(a){
+			a.target = '_blank';
+		})
+		console.log('add link target')
+
+	},
+	remoteLinkTarget() {
+		[].forEach.call($('a'),function(a){
+			a.target = '_self';
+		})
+		console.log('remote link target')
+	},
+}
+
+chrome.extension.onMessage.addListener(function(data) {
+	console.log(data+':'+functionHash.hasOwnProperty(data))
+	if(functionHash.hasOwnProperty(data)){
+		var executeFun = functionHash[data]
+		executeFun();
+	}
 });
 
-function changeColor(color) {
-	document.body.style.backgroundColor = color;
-};
--- popup.js
+++ popup.js
@@ -1,29 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
   var data = '?'
   document.getElementById('box1').onclick = function() {
-    data = 'red';
+    data = 'outlineFun';
     //chrome.runtime.sendMessage('by runtime.data:' + data, sender, sendResponse);
     sendData(data)
-
   }
   document.getElementById('box2').onclick = function() {
-    data = 'blue';
+    data = 'addLinkTarget';
     sendData(data)
   }
   document.getElementById('box3').onclick = function() {
-    data = 'green';
+    data = 'remoteLinkTarget';
     sendData(data)
   }
   document.getElementById('box4').onclick = function() {
     data = 'yellow';
-    sendData(data)
   }
 
   function sendData(data) {
     chrome.tabs.query({
       active: true
     }, function(tab) {
-      chrome.tabs.sendMessage(tab[0].id, 'hello, content script, from background page. there are some data:' + data);
+      chrome.tabs.sendMessage(tab[0].id, data);
     });
   };
 })
